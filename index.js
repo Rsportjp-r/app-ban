95f8bd98f92d177a125b617a7674c9b05565d18d
Rsportjp-r <111325236+Rsportjp-r@users.noreply.github.com>
2022-08-16 18:43:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const express = require('express')();
+express.get('/', (req, res) => res.send('<!-- auto-pinging 525316393768452098 -->'))
+express.listen(3000);
+
 const mineflayer = require('mineflayer')
+const sleep = require('util').promisify(setTimeout);
 const bot = mineflayer.createBot({
   host: 'play.parsermc.ga', // minecraft server ip
   username: 'uptimer', // minecraft username
@@ -9,6 +14,7 @@ const bot = mineflayer.createBot({
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // auth: 'mojang'              // only set if you need microsoft auth, then set this to '
 })
+
 
 
 
